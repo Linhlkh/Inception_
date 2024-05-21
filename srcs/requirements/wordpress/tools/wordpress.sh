50cb7362829f6a7236ada0af8fd021b677990849
Khanh linh LE <khle@paul-f4Br3s1.clusters.42paris.fr>
2024-05-21 08:29:48 +0200

    path

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-# sleep 10
+sleep 10
 
-# cd /var/www/wordpress
+cd /var/www/wordpress
 
-# wp config create	--allow-root \
-# 					--dbname=$SQL_DATABASE \
-# 					--dbuser=$SQL_USER \
-# 					--dbpass=$SQL_PASSWORD \
-# 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 					
 
-# wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/wordpress'
+wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/wordpress'
 
-# wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> log.txt
+wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> log.txt
 
-# if [ ! -d /run/php ]; then
-#     mkdir -p /run/php
-# fi
+if [ ! -d /run/php ]; then
+    mkdir -p /run/php
+fi
 
-# /usr/sbin/php-fpm7.3 -F
-
-exec "$@"
+/usr/sbin/php-fpm7.3 -F
