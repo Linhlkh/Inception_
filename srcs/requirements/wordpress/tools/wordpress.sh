7d9f7ef81a0a21395ad99da68b83843f8b2fd859
Khanh linh LE <khle@paul-f4Br3s1.clusters.42paris.fr>
2024-05-21 06:54:12 +0200

    pathh

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,7 +6,9 @@ wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+					--dbhost=mariadb:3306 --path='/var/www/wordpress' \
+					--force
+
 
 wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 
