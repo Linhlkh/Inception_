ecb32d350b003fc2aa3b8d9618a8114b47fd6530
Khanh linh LE <khle@paul-f4Br3s1.clusters.42paris.fr>
2024-05-21 08:02:05 +0200

    path

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -8,8 +8,8 @@ wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress' \
-				
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+					
 
 wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/wordpress'
 
